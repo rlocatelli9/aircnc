1f8c8b89176cc51ba14f62af3ffff55691e93037
Rodrigo Petry <rodrigo.petry18@gmail.com>
2019-10-09 16:23:15 -0300

    Ajustando Erros de Cors

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://rodrigo:gqwabc123@omnistack-5l5vj.mongodb.net/se
 // req.params = Acessar Route Params, (Para edição e delete) Ex: /users/1
 // req.body = Acessar Corpo da Requisição (Para criação e edição)
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
